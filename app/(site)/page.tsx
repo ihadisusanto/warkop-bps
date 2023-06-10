26a729e203768f02e7435bbfff26580494ebf865
Hamam Hadinata W <222011532@stis.ac.id>
2023-06-10 16:52:06 +0700

    push isian page site

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -6,11 +6,17 @@ export default function Home() {
   return (
     <>
     <Header/>
-    <div className="min-h-full"
-      >
-      <ChatSys/>
+    <div className="my-24 px-8 pt-16 pb-6">
+      <h4 className="text-darkbrown text-2xl font-semibold">Selamat Datang di</h4>
+      <h1 className="text-secondaryBrown font-bold uppercase text-4xl my-3">Warung Kompetensi Pegawai</h1>
+      <p className="text-darkbrown text-justify"><strong>Menyajikan berbagai menu pengembangan kompetensi </strong>dengan memadukan media digital dan teknologi daring, keterbatasan pegawai dalam mengembangkan kompetensi karena jarak dan waktu dapat diminimalkan. Sesuai dengan namanya, Warkop menyediakan berbagai menu pembelajaran pengembangan kompetensi yang dapat dipilih oleh "pelanggan".</p>
+      <h3 className="text-secondaryBrown font-bold text-center text-3xl mt-20">Pengenalan WARKOP</h3>
+    </div>
+    <div className="bg-white">
+
     </div>
     <Footer2nd/>
+    <ChatSys/>
     </>
   )
 }
--- app/components/footer2nd/page.tsx
+++ app/components/footer2nd/page.tsx
@@ -38,7 +38,7 @@ export default function Footer2nd() {
                         <path d="M1.5 8.67v8.58a3 3 0 003 3h15a3 3 0 003-3V8.67l-8.928 5.493a3 3 0 01-3.144 0L1.5 8.67z" />
                         <path d="M22.5 6.908V6.75a3 3 0 00-3-3h-15a3 3 0 00-3 3v.158l9.714 5.978a1.5 1.5 0 001.572 0L22.5 6.908z" />
                     </svg>
-                    E-mail:&ensp;<a href="pusdiklat.bps.go.id">pusdiklat.bps.go.id</a>
+                    E-mail:&ensp;<a href="pusdiklat.bps.go.id" className="hover:text-primary hover:underline">pusdiklat.bps.go.id</a>
                 </li>
             </ul>
         </div>
