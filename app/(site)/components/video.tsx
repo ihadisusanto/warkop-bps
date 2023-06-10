19c37a4c51370ac87e4d7041d5bc2adc159658a2
susanto_m <222011324@stis.ac.id>
2023-06-10 19:34:08 +0700

    clipRule fixed

--- app/(site)/components/video.tsx
+++ app/(site)/components/video.tsx
@@ -5,7 +5,8 @@ export default function Video() {
             <p className="text-darkbrown text-justify lg:text-center">Warkop hadir untuk mengakomodir seluruh kegiatan pendidikan dan pelatihan baik diklat prajabatan dan kepemimpinan maupun diklat teknis dan fungsional.</p>
         </div>
         <div className="w-full lg:w-1/2 h-1/2 mt-4 mx-auto">
-            <iframe className="w-full aspect-video items-center" src="@public/video/Warkop.mp4"></iframe>
+            <iframe className="w-full aspect-video items-center" src="../../../public/video/Warkop.mp4"></iframe>
+            {/* <video src={require('../../../public/video/Warkop.mp4')} /> */}
         </div>
     </div>
     )
